e60434f4842c7edc23241064a04783b32b2dad17
Harshul Dashora <harshuldashora01@gmail.com>
2026-05-29 16:56:25 +0530

    Fix Streamlit secrets

--- utils/llm.py
+++ utils/llm.py
@@ -1,28 +1,20 @@
 from groq import Groq
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+import streamlit as st
 
 client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key=st.secrets["GROQ_API_KEY"]
 )
 
 def ask_llm(prompt):
 
-    try:
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
 
-        response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
-        )
-
-        return response.choices[0].message.content
-
-    except Exception as e:
-        return f"Error: {str(e)}"
+    return response.choices[0].message.content
